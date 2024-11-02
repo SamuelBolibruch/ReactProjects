88c65977b2aabd41cafa7a07da668b22c056d396
Samuel Bolibruch <samo.bolibruch@gmail.com>
2024-11-02 14:28:27 +0100

    Image Slider App

--- 25-react-projects/src/App.js
+++ 25-react-projects/src/App.js
@@ -1,14 +1,16 @@
-import Accordian from './components/accordian';
-import RandomColor from './components/random-color';
-import StarRating from './components/star-rating';
-import './App.css'
+import Accordian from "./components/accordian";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
+import ImageSlider from "./components/image-slider";
+import "./App.css";
 
 function App() {
   return (
     <>
-    {/* <Accordian/> */}
-    {/* <RandomColor/> */}
-    <StarRating numberOfStars={10}/>
+      {/* <Accordian/> */}
+      {/* <RandomColor/> */}
+      {/* <StarRating numberOfStars={10}/> */}
+      <ImageSlider url={"https://picsum.photos/v2/list"} limit={"10"} page={1} />
     </>
   );
 }
