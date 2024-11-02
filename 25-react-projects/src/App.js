ff0dcb3f2a5bd8ea38735477f47c50a950f89e89
Samuel Bolibruch <samo.bolibruch@gmail.com>
2024-11-02 12:08:12 +0100

    Random Color App

--- 25-react-projects/src/App.js
+++ 25-react-projects/src/App.js
@@ -1,9 +1,12 @@
 import Accordian from './components/accordian';
+import RandomColor from './components/random-color';
+import './App.css'
 
 function App() {
   return (
     <>
-    <Accordian/>
+    {/* <Accordian/> */}
+    <RandomColor/>
     </>
   );
 }
